ae4a4e9d83d695e8fed9a6e02a7bd34544e0dab1
carloscavalcante <carlos.xande115@gmail.com>
2021-07-14 10:48:16 -0300

    Create audiototext.py

---
+++ audiototext.py
@@ -0,0 +1,22 @@
+import speech_recognition as sr
+from translate import Translator
+
+arquivo="audio.wav"
+
+#Funcao responsavel por ouvir e reconhecer a fala
+def ouvir_microfone():
+     #Habilita a gravacao para utilizacao no Speech
+     r = sr.Recognizer()
+     microfone = sr.AudioFile(arquivo)
+     #Passa o audio para o reconhecedor de padroes do speech_recognition
+     with microfone as source:
+      audio = r.record(source)
+      try:
+        frase = r.recognize_google(audio,language='pt-BR')
+      #Caso nao tenha reconhecido o padrao de fala, exibe esta mensagem
+      except sr.UnkownValueError:
+        frase='Audio não legível'
+     return frase
+
+#retornou o texto do audio enviado
+fala=ouvir_microfone()
